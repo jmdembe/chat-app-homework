dfc21e3aaa5c5ed4c25ae8b35a2066051a075b62
Jessica Dembe <jmdembe@gmail.com>
2016-11-02 16:09:26 -0400

    Added functionality to receive messages

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -1,16 +1,12 @@
 (function() {
     'use strict';
-    window.chat  = window.chat || {}
+    window.chat  = window.chat || {};
 
-
-    window.chat.listenForMessages(function messageHandler(event) {
-        var chatMessage = $('input.message').val();
-
-
-     });//end of listenForMessages
+    var token;
 
      $('.login')
          .on('submit', function getLogin(event) {
+           console.log('Im in the submit');
            var userName = $('.username').val();
            event.preventDefault();
              $.ajax({
@@ -19,17 +15,50 @@
                  data: JSON.stringify({ username: userName}),
                  headers: {
                      'Content-Type': 'application/json',
-                 }//end of headers
+                 }//end of headers for getLogin
                })//end of ajax call for login
-             .done (function handleChatMessageSuccess(data){
+             .done (function handleUsernameSuccess(data){
                  console.log("I MADE IT!");
                  console.log(data);
+                 token = data.token;
              })//end of done callback
-             .fail (function handleChatMessageError(xhr){
+             .fail (function handleUsernameError(xhr){
                  console.log(xhr);
              })//end of fail callback
          })//ends event handler
 
 
+       $('.send-message')
+          .on('submit', function getMessage(event) {
+              event.preventDefault();
+              var chatMessage = $('.message').val();
+              event.preventDefault();
+                  $.ajax({
+                      url: '/chat',
+                      method: 'POST',
+                      data: JSON.stringify({ message: chatMessage}),
+                      headers:{
+                          'Authorization': token,
+                      }//ends headers for getMessage
+                    })
+                    .done (function handleMessageSuccess (data) {
+                        console.log('Message success');
+                    })//ends callback function for getMessage success
+                    .fail (function handleMessageError (xhr) {
+                        console.log('Error', xhr);
+                    })//ends callback for getMessage fail
+
+
+          })//ends getMessage event handler
+
+
+
+
+
+      window.chat.listenForMessages(function messageHandler(event) {
+          var chatMessage = $('input.message').val();
+
+
+          });//end of listenForMessages
 
 }());
